f1fb7e80f5ada410a2776dd84699939202a54b93
Alex <50113949+Dudarik@users.noreply.github.com>
2024-09-01 09:28:58 +0500

    refactor: add Header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
-import Button from 'primevue/button';
-import { onMounted } from 'vue';
-
-const tg = window.Telegram.WebApp;
-console.log('window.Telegram.WebApp', window.Telegram.WebApp);
-onMounted(() => {
-  tg.ready();
-});
-const onClose = () => {
-  tg.close();
-};
+import Header from './components/Header.vue';
 </script>
 
 <template>
   <div class="container">
-    <Button label="close" @click="onClose" />
+    <Header />
   </div>
 </template>
 
---
+++ client/src/components/Header.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Button from 'primevue/button';
+import { onMounted } from 'vue';
+
+const tg = window.Telegram.WebApp;
+console.log('window.Telegram.WebApp', window.Telegram.WebApp);
+onMounted(() => {
+  tg.ready();
+});
+const onClose = () => {
+  tg.close();
+};
+</script>
+<template>
+  <Header>
+    <div class="user_info">{{ tg.initDataUnsafe.user?.username }}</div>
+    <Button label="close" @click="onClose" />
+  </Header>
+</template>
+
+<style scoped></style>
